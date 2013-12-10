4bbe46db6e6ff190ebdfe14d8b7fbbaa2052dcd6
Dan Hassin <danhassin@mac.com>
2013-12-09 23:49:27 -0500

    Move some formatting from model to helper

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -7,7 +7,46 @@ module MainHelper
 		end
 	end
 
-	def formatted_prereqs(course)
-		raw(course.prereqs.gsub(/([A-Za-z]*\s+\d+[A-Za-z]*)/, inline_form.strip)) #strip off some whitespace that seems to come w the form
+	def format_courselist(txt)
+		raw txt.gsub(/([A-Za-z]*\s*\d+[A-Za-z]*)/, inline_form.strip) #strip off some whitespace that seems to come w the form
+	end
+
+	def format_instructors(txt)
+		raw txt.split("; ").map {|i| "<a>#{i}</a>"}.join("; ")
+	end
+
+	def enroll_bar_style(course)
+		return "success" if course.enroll_percent < 75 || course.no_cap?
+		return "warning" if course.enroll_percent < 90
+		return "danger"
+	end
+
+	def enroll_bar_precentage(course)
+		return 100 if course.no_cap?
+		course.enroll_percent
+	end
+
+	def format_time(time)
+		if time.size == 3 #940
+			hour = time[0].to_i #9
+			mins = time[1..2] #40
+		else #1050
+			hour = time[0..1].to_i #10
+			mins = time[2..3] #50
+		end
+		am = hour < 12
+		if hour > 12
+			hour = "#{hour - 12}"
+		end
+		"#{hour}:#{mins}#{am ? "am" : "pm"}"
+	end
+
+	def formatted_time_and_day(course)
+		days_of_week = {"M" => "Mon", "T" => "Tues", "W" => "Wed", "R" => "Thurs", "F" => "Fri"}
+		# days is like "MWF". split into chars, map each one to the longer version, join with /, so Mon/Wed/Fri
+		d = course.days.split("").map {|d| days_of_week[d]}.join("/")
+		s = format_time(course.start_time.to_s)
+		e = format_time(course.end_time.to_s)
+		"#{d} #{s}-#{e}"
 	end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,46 +1,12 @@
 class Course < ActiveRecord::Base
 	belongs_to :department
 
+	def no_cap?
+		cap == 0
+	end
+
 	def enroll_percent
-		return 100 if cap == 0
 		enroll*100.0/cap
-	end
-
-	def bar_style
-		return "success" if enroll_percent < 75 || cap == 0
-		return "warning" if enroll_percent < 90
-		"danger"
-	end
-
-	def format_time(time)
-		if time.size == 3
-			hour = time[0].to_i
-			mins = time[1..2]
-		else
-			hour = time[0..1].to_i
-			mins = time[2..3]
-		end
-		am = true
-		if hour > 12
-			hour = "#{hour - 12}"
-			am = false
-		end
-		"#{hour}:#{mins}#{am ? "am" : "pm"}"
-	end
-
-	def formatted_time
-		d = days.split("").map {|d| {"M" => "Mon", "T" => "Tues", "W" => "Wed", "R" => "Thurs", "F" => "Fri"}[d] }.join("/")
-		s = format_time(start_time.to_s)
-		e = format_time(end_time.to_s)
-		"#{d} #{s}-#{e}"
-	end
-
-	def formatted_instructors
-		instructors.split("; ").map {|i| "<a>#{i}</a>"}.join("; ")
-	end
-
-	def formatted_cross_listed
-		cross_listed.gsub(/([A-Za-z]*\s+\d+[A-Za-z]*)/, '<a>\1</a>')
 	end
 
 	def time_tba?

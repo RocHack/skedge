7d3e5f3e8d40b1c5d1eae9094d52072a2a17f5a7
Dan Hassin <danhassin@mac.com>
2013-12-10 00:52:29 -0500

    Clean up search/controller code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,18 +1,26 @@
 class MainController < ApplicationController
-	def index
-		query = params[:query]
-		if query.present?
-			dept = Department.where('short LIKE ?', "%#{query}%").first
-			if dept
-				@courses = dept.courses
-			elsif (match = query.match /^([A-Za-z]*)\s*(\d+)/)
-				dept = Department.where('short LIKE ?', "%#{match[1]}%").first
-				@courses = Course.where({num:match[2].to_i, department:dept})
-			else
-				@courses = Course.where('name LIKE ?', "%#{query}%")
-			end
-		else
-			@courses = []
+	def search_for_courses(query)
+		return [] if !query
+
+		query.strip!
+
+		# check for a dept, (case insensitive)
+		if query.size <= 3
+			dept = Department.where("lower(short) = ?", query.downcase).first
+			return dept.courses if dept
 		end
+
+		# check for a specific course (CSC 171)
+		if (match = query.match /^([A-Za-z]*)\s*(\d+[A-Za-z]*)/)
+			dept = Department.where("lower(short) = ?", match[1].downcase).first
+			return Course.where({num:match[2].to_i, department:dept})
+		end
+
+		# otherwise just search titles
+		Course.where("lower(name) = ?", query.downcase).first
+	end
+
+	def index
+		@courses = search_for_courses(params[:query])
 	end
 end

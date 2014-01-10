08dc064b54d006f7cdf25cecb09d52181fa95a3a
Dan Hassin <danhassin@mac.com>
2014-01-09 22:59:24 -0500

    Subcourse partials unfortunately were a performance hit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Skedge::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #config.log_level = :warn
 end

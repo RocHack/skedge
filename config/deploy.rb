06234edb4879879696e27ad3072a27ed170b9d57
Dan Hassin <danhassin@mac.com>
2014-03-16 05:24:55 -0400

    Another try at og... last one

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :pty, true
 
 set :user, 'deploy'
 
-set :linked_files, %w{config/database.yml}
+#set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
